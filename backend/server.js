132c85cddcf26c2ccef8850ad18e1afb3e26191d
Junaid Salie <jsalie165@gmail.com>
2025-01-27 23:03:00 +0200

    remove port

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,13 @@ import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
+import cors from "cors";
 import connectDB from "./config/db.js";
 import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import userRoutes from "./routes/userRoutes.js";
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 
 connectDB();
 
@@ -16,13 +17,15 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(cors());
+
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
