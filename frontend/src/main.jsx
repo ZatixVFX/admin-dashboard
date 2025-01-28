0a75a8eb2ead6ec31f557ff5b16b76207ff6b9a8
Junaid Salie <jsalie165@gmail.com>
2025-01-28 23:04:47 +0200

    change title

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -25,7 +25,6 @@ const { userInfo } = store.getState().auth;
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
-      <Route path="/" element={<Navigate to="/login" />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
       <Route path="" element={<Dashboard />}>
